0733ef14d5f0322d291bc191d3272c60622f24ec
ashokb <ashokb@yellowblock.net>
2024-02-03 16:43:40 +0530

    shimmers

--- src/components/Dao/shimmers/proposalcardshimmer.tsx
+++ src/components/Dao/shimmers/proposalcardshimmer.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 
 const ProposalCardShimmer = () => {
-  return (
+  return (<>
     <div className="animate-pulse space-x-4">
       <div className=" w-full opacity-1 border rounded-xl gap-10 flex items-center p-4 ">
-        <div className="w-full md:h-[400px] rounded-md bg-slate-200  "></div>
+        <div className="w-full md:h-[208px] rounded-md bg-slate-200  "></div>
       </div>
     </div>
+    <div className="grid grid-cols-12 gap-4 mt-5">
+    <div className="animate-pulse space-x-4 col-span-4">
+      <div className=" w-full opacity-1 rounded-xl gap-10 flex items-center p-4 ">
+        <div className="w-12 h-12  rounded-full bg-slate-200  shrink-0"></div>        
+        <div className="w-full opacity-1 rounded-xl">
+        <div className="w-full h-5  rounded-full bg-slate-200  mb-2"></div>        
+        <div className="w-full h-5  rounded-full bg-slate-200  "></div>   
+          </div>     
+      </div>
+      <div className="w-full opacity-1 rounded-xl p-4">
+      <div className="w-full h-5  rounded-full bg-slate-200  ">
+        </div>  
+        </div>  
+    </div>
+    <div className="animate-pulse space-x-4 col-span-8">
+      <div className=" w-full opacity-1 border rounded-xl gap-10 flex items-center p-4 ">
+        <div className="w-full md:h-[288px] rounded-md bg-slate-200  "></div>
+      </div>
+    </div>
+    </div>
+    </>
   );
 };
 
