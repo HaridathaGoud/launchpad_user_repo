3d761dc70a99e2d77e99a7beaa1ec19a27e01629
Mouneeswar <Mouneeswar.m@yellowblock.net>
2024-02-24 12:29:40 +0530

    changes in kyc tab to not show invalid date and idenfication details if not present

--- src/components/profile/kycDetails.tsx
+++ src/components/profile/kycDetails.tsx
@@ -11,10 +11,10 @@ const KycDetails = ({ kycStatus, id }) => {
   const { setErrorMessage }: OutletContextModel = useContext(outletContext);
   const [userDetails, setUserDetails] = useState(null);
   const [identityDetails, setIdentityDetails] = useState(null);
-  const shouldLog=useRef(true)
+  const shouldLog = useRef(true);
   useEffect(() => {
     if (id && shouldLog.current) {
-      shouldLog.current=false;
+      shouldLog.current = false;
       getCustomerKycDetail(id);
     }
   }, [id]); // eslint-disable-line react-hooks/exhaustive-deps
@@ -33,16 +33,15 @@ const KycDetails = ({ kycStatus, id }) => {
           acc[key].push(obj);
           return acc;
         }, {});
-        const tableData = result ? Object.entries(result): null;
+        const tableData = result ? Object.entries(result) : null;
         setIdentityDetails(tableData);
-      }
-      else{
+      } else {
         setErrorMessage?.(response);
       }
     } catch (error) {
-        console.log(error)
+      console.log(error);
       setErrorMessage?.(error);
-    } 
+    }
   };
   const navigateToKyc = () => {
     navigate("/completeKyc");
@@ -80,164 +79,159 @@ const KycDetails = ({ kycStatus, id }) => {
     return <h4 className="text-center">No Data</h4>;
   }
   return (
-      <>
-        <div className="">
-          <h2 className="font-semibold text-lg text-secondary mb-4">
-            Personal
-          </h2>
-          {
-            <>
-              {userDetails && (
-                <>
-                  <div className="grid md:grid-cols-4 mb-4">
-                    <div className="col">
-                      <p className="text-sm font-normal text-secondary opacity-[0.9]">
-                        First Name{" "}
-                      </p>
-                    </div>
-                    <div className="md:col-span-3">
-                      <p className="font-medium text-sm text-secondary">
-                        {userDetails?.firstName || "--"}
-                      </p>
-                    </div>
+    <>
+      <div className="">
+        <h2 className="font-semibold text-lg text-secondary mb-4">Personal</h2>
+        {
+          <>
+            {userDetails && (
+              <>
+                <div className="grid md:grid-cols-4 mb-4">
+                  <div className="col">
+                    <p className="text-sm font-normal text-secondary opacity-[0.9]">
+                      First Name{" "}
+                    </p>
                   </div>
-                  <div className="grid md:grid-cols-4 mb-4">
-                    <div className="">
-                      <p className="text-sm font-normal text-secondary opacity-[0.9]">
-                        Last Name
-                      </p>
-                    </div>
-                    <div className="md:col-span-3">
-                      <p className="font-medium text-sm text-secondary">
-                        {userDetails?.lastName || "--"}
-                      </p>
-                    </div>
+                  <div className="md:col-span-3">
+                    <p className="font-medium text-sm text-secondary">
+                      {userDetails?.firstName || "--"}
+                    </p>
                   </div>
-                  <div className="grid md:grid-cols-4 mb-4">
-                    <div className="">
-                      <p className="text-sm font-normal text-secondary opacity-[0.9]">
-                        Email
-                      </p>
-                    </div>
-                    <div className="md:col-span-3">
-                      <p className="font-medium text-sm text-secondary">
-                        {userDetails?.email || "--"}
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="grid md:grid-cols-4 mb-4">
-                    <div className="">
-                      <p className="text-sm font-normal text-secondary opacity-[0.9]">
-                        Phone No
-                      </p>
-                    </div>
-                    <div className="md:col-span-3">
-                      <p className="font-medium text-sm text-secondary">
-                        {userDetails?.phoneNo || "--"}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="grid md:grid-cols-4 mb-4">
-                    <div className="">
-                      <p className="text-sm font-normal text-secondary opacity-[0.9]">
-                        Date Of Birth
-                      </p>
-                    </div>
-
-                    <div className="md:col-span-3">
-                      <p className="font-medium text-sm text-secondary">
-                        <Moment format="DD/MM/YYYY">
-                          {userDetails?.dob || "--"}
-                        </Moment>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="grid md:grid-cols-4 mb-4">
-                    <div className="">
-                      <p className="text-sm font-normal text-secondary opacity-[0.9]">
-                        Country
-                      </p>
-                    </div>
-                    <div className="md:col-span-3">
-                      <p className="font-medium text-sm text-secondary">
-                        {userDetails?.country || "--"}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="grid md:grid-cols-4 mb-4">
-                    <div className="">
-                      <p className="text-sm font-normal text-secondary opacity-[0.9]">
-                        Discord ID
-                      </p>
-                    </div>
-                    <div className="md:col-span-3">
-                      <p className="font-medium text-sm text-secondary">
-                        {userDetails?.discordId || "--"}
-                      </p>
-                    </div>
-                  </div>
-                </>
-              )}
-            </>
-          }
-        </div>
-
-        <div className="">
-          <h2 className="font-semibold text-lg text-secondary mb-3">
-            Identification
-          </h2>
-          <div className="grid lg:grid-cols-5 gap-4">
-            {identityDetails?.map(([idType, items]) => (
-              <div className="">
-                <p className="text-sm font-normal text-secondary opacity-[0.9] mb-1">
-                  {idType === "PASSPORT"
-                    ? "Passport"
-                    : idType === "SELFIE"
-                    ? "Selfie"
-                    : idType === "DRIVERS"
-                    ? "Driver's License"
-                    : idType === "ID_CARD"
-                    ? "ID Card"
-                    : idType === "RESIDENCE_PERMIT"
-                    ? "Residence Permit"
-                    : ""}
-                </p>
-                <div className="">
-                  {items?.map((item: any) => (
-                    <>
-                      <div
-                        className={`${
-                          item?.idType === "SELFIE" ? "avatar" : ""
-                        } w-full overflow-hidden rounded-xl`}
-                      >
-                        <div
-                          className={`w-full md:h-64 rounded-xl max-sm:h-64  ${
-                            item?.idType === "SELFIE"
-                              ? ""
-                              : "flex items-center bg-slate-300"
-                          }`}
-                        >
-                          <img
-                            className={`${
-                              item?.idType === "SELFIE"
-                                ? "object-cover rounded-xl max-sm:h-64"
-                                : "object-contain max-sm:h-64"
-                            }`}
-                            src={item?.url}
-                            alt={item.idType}
-                          />
-                        </div>
-                      </div>
-                    </>
-                  ))}
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </>
+                <div className="grid md:grid-cols-4 mb-4">
+                  <div className="">
+                    <p className="text-sm font-normal text-secondary opacity-[0.9]">
+                      Last Name
+                    </p>
+                  </div>
+                  <div className="md:col-span-3">
+                    <p className="font-medium text-sm text-secondary">
+                      {userDetails?.lastName || "--"}
+                    </p>
+                  </div>
+                </div>
+                <div className="grid md:grid-cols-4 mb-4">
+                  <div className="">
+                    <p className="text-sm font-normal text-secondary opacity-[0.9]">
+                      Email
+                    </p>
+                  </div>
+                  <div className="md:col-span-3">
+                    <p className="font-medium text-sm text-secondary">
+                      {userDetails?.email || "--"}
+                    </p>
+                  </div>
+                </div>
 
+                <div className="grid md:grid-cols-4 mb-4">
+                  <div className="">
+                    <p className="text-sm font-normal text-secondary opacity-[0.9]">
+                      Phone No
+                    </p>
+                  </div>
+                  <div className="md:col-span-3">
+                    <p className="font-medium text-sm text-secondary">
+                      {userDetails?.phoneNo || "--"}
+                    </p>
+                  </div>
+                </div>
+                <div className="grid md:grid-cols-4 mb-4">
+                  <div className="">
+                    <p className="text-sm font-normal text-secondary opacity-[0.9]">
+                      Date Of Birth
+                    </p>
+                  </div>
+
+                  <div className="md:col-span-3">
+                    <p className="font-medium text-sm text-secondary">
+                      {userDetails?.dob ? <Moment format="DD/MM/YYYY">
+                        {userDetails?.dob }
+                      </Moment> : "--"}
+                    </p>
+                  </div>
+                </div>
+                <div className="grid md:grid-cols-4 mb-4">
+                  <div className="">
+                    <p className="text-sm font-normal text-secondary opacity-[0.9]">
+                      Country
+                    </p>
+                  </div>
+                  <div className="md:col-span-3">
+                    <p className="font-medium text-sm text-secondary">
+                      {userDetails?.country || "--"}
+                    </p>
+                  </div>
+                </div>
+                <div className="grid md:grid-cols-4 mb-4">
+                  <div className="">
+                    <p className="text-sm font-normal text-secondary opacity-[0.9]">
+                      Discord ID
+                    </p>
+                  </div>
+                  <div className="md:col-span-3">
+                    <p className="font-medium text-sm text-secondary">
+                      {userDetails?.discordId || "--"}
+                    </p>
+                  </div>
+                </div>
+              </>
+            )}
+          </>
+        }
+      </div>
+
+     {identityDetails && <div className="">
+        <h2 className="font-semibold text-lg text-secondary mb-3">
+          Identification Documents
+        </h2>
+        <div className="grid lg:grid-cols-5 gap-4">
+          {identityDetails?.map(([idType, items]) => (
+            <div className="" key={idType}>
+              <p className="text-sm font-normal text-secondary opacity-[0.9] mb-1">
+                {idType === "PASSPORT"
+                  ? "Passport"
+                  : idType === "SELFIE"
+                  ? "Selfie"
+                  : idType === "DRIVERS"
+                  ? "Driver's License"
+                  : idType === "ID_CARD"
+                  ? "ID Card"
+                  : idType === "RESIDENCE_PERMIT"
+                  ? "Residence Permit"
+                  : ""}
+              </p>
+              <div className="">
+                {items?.map((item: any) => (
+                  <div
+                    className={`${
+                      item?.idType === "SELFIE" ? "avatar" : ""
+                    } w-full overflow-hidden rounded-xl`}
+                  >
+                    <div
+                      className={`w-full md:h-64 rounded-xl max-sm:h-64  ${
+                        item?.idType === "SELFIE"
+                          ? ""
+                          : "flex items-center bg-slate-300"
+                      }`}
+                    >
+                      <img
+                        className={`${
+                          item?.idType === "SELFIE"
+                            ? "object-cover rounded-xl max-sm:h-64"
+                            : "object-contain max-sm:h-64"
+                        }`}
+                        src={item?.url}
+                        alt={item.idType}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>}
+    </>
   );
 };
 
