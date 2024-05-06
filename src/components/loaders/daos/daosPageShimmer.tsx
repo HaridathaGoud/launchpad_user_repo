bdb183ada42abd2fd7097ecf372c5260005feec7
Mouneeswar <Mouneeswar.m@yellowblock.net>
2024-05-06 09:59:15 +0530

    changesin dao page shimmer

--- src/components/loaders/daos/daosPageShimmer.tsx
+++ src/components/loaders/daos/daosPageShimmer.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import DaoCardShimmer from '../../Dao/shimmers/daodashboard'
+import React from "react";
+import DaoCardShimmer from "../../Dao/shimmers/daodashboard";
 
 const DaosPageShimmer = () => {
   return (
     <div className="container mx-auto px-3 lg:px-0 mt-4">
-         {[...Array(8)].map((_, index) => (
-              <div key={index}>
-                <DaoCardShimmer />
-              </div>
-            ))}
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {[...Array(8)].map((_, index) => (
+          <div key={index}>
+            <DaoCardShimmer />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DaosPageShimmer
+export default DaosPageShimmer;
