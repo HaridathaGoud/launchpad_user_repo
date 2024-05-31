179a55321c3c725cae617f8b12b43dfd0c577a91
ashokb <ashokb@yellowblock.net>
2024-05-31 14:59:54 +0530

    about-banner

--- src/components/strapi/aboutus/index.tsx
+++ src/components/strapi/aboutus/index.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
 import {  useParams } from 'react-router-dom'; // Import useHistory from react-router-dom
 import Button from '../../../ui/Button';
+import abstract1 from '../../../assets/images/about-abstarct1.svg'
+import abstract2 from '../../../assets/images/about-abstarct2.svg'
  
 function AboutUs() {
   const [postDetails,setPost] = useState([]);
@@ -31,31 +33,36 @@ console.log(params);
  
   return (
     <div>
-       {postDetails?.data?.map((item) => (<>       
+       {postDetails?.data?.map((item) => (<>  
+       <div className="bg-[#E7F4FA] bg-about relative">     
         <div className='container mx-auto grid md:grid-cols-2 items-center'>
          <div>
-         <h1 className=''>{item.attributes.abouttitle} <span>{item.attributes.primarytitle}</span> {item.attributes.suffixtitle} </h1>
-         <p>{item.attributes.ybDescription}</p>
-          <div className="flex">
+         <h1 className='text-[40px] font-semibold text-secondary'>{item.attributes.abouttitle} <span className='text-primary'>{item.attributes.primarytitle}</span> {item.attributes.suffixtitle} </h1>
+         <p className='text-base text-base-200 font-normal mt-4'>{item.attributes.ybDescription}</p>
+          <div className="flex gap-16 my-6">
             <div>
-              <h1>{item.attributes.expvalue}</h1>
-              <p>{item.attributes.explabel}</p>
+              <h1 className='text-[48px] font-semibold text-secondary'>{item.attributes.expvalue}</h1>
+              <p className='text-base font-medium text-primary'>{item.attributes.explabel}</p>
             </div>
             <div>
-              <h1>{item.attributes.devvalue}</h1>
-              <p>{item.attributes.devlabel}</p>
+              <h1 className='text-[48px] font-semibold text-secondary'>{item.attributes.devvalue}</h1>
+              <p className='text-base font-medium text-primary'>{item.attributes.devlabel}</p>
             </div>
           </div>
-          <Button type='primary' children={item.attributes.contact.Title}  />
-          <Button type='cancel' children={item.attributes.knowmore.Title} btnClassName='ml-4'  />
+          <Button type='primary' children={item.attributes.contact.Title} btnClassName='md:w-[160px]'  />
+          <Button type='cancel' children={item.attributes.knowmore.Title} btnClassName='ml-4 md:w-[160px] whitespace-nowrap'  />
          </div>
           <div> 
           <img src={'http://localhost:1337' + item.attributes.bannerimg.data.attributes.url} alt="" width='' className='md:w-[564px] ml-auto' />          
           </div>
         </div>
+        <img src={abstract1} alt="" className='absolute bottom-[-180px]' />
+        <img src={abstract2} alt="" className='absolute right-0 top-56' />
+        </div>
         <div className='grid md:grid-cols-2 items-center'>
             
         </div>
+        
         </>
 
       ))}
