a1b6253ae8e3255c26dc8cb5c1905410c914a6da
haridatha <haridatta@yellowblock.net>
2024-06-03 15:22:05 +0530

    changes   in dateformat

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,7 +1,8 @@
 import moment from 'moment';
- const ConvertLocalFormat = (item:any) => {
+
+const ConvertLocalFormat = (item: any) => {
   if (!item) return null;
   const localDateTime = moment(item).add(5.5, 'hours');
-  return localDateTime.format("DD-MM-YYYY HH:mm");
+  return localDateTime.format("DD-MM-YYYY hh:mm A");
 }
 export default ConvertLocalFormat;
