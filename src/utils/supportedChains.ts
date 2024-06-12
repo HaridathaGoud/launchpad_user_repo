a4190067da6ade698c56569d5cc803f7208da9c2
Mouneeswar <Mouneeswar.m@yellowblock.net>
2024-06-12 09:25:24 +0530

    changes in supported chains

--- src/utils/supportedChains.ts
+++ src/utils/supportedChains.ts
@@ -1,8 +1,5 @@
-const supportedChains = [
-    80002,
-    0x13882,
-    137,
-    0x89
-
-]
-export { supportedChains }
+const supportedChains =
+  process.env.REACT_APP_ENV_VAR === "prd"
+    ? [137, 0x89]
+    : [80002, 0x13882, 137, 0x89];
+export { supportedChains };
