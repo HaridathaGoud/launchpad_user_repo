5451f5288c66a66a9cef15c6654b8929a99a3bad
ashokb <ashokb@yellowblock.net>
2024-06-22 14:19:51 +0530

    min-max-design

--- src/components/marketplace.component/hotcollections.component/detailviewstatus.tsx
+++ src/components/marketplace.component/hotcollections.component/detailviewstatus.tsx
@@ -57,11 +57,16 @@ const StatusDetailview = ({
                     </ul>
                 </div>
                 <div className="join w-full mt-4">
-                    <button className=" join-item bg-info-content border-0 text-dark hover:bg-info-content rounded-tl-[25px] rounded-bl-[25px] text-sm font-normal !border-0 flex-1 py-2.5 px-[14px] text-left" 
-                    onClick={()=>sendSelectedValue("Min")}>Min</button>
-                    <span className=" join-item bg-info-content border-0 text-dark hover:bg-info-content font-medium !border-0 shrink-0 px-1 pt-2">to</span>
-                    <button className=" join-item bg-info-content border-0 text-dark hover:bg-info-content rounded-tr-[25px] rounded-br-[25px] text-sm font-normal flex-1 !border-0 px-[14px] text-right" 
+                    <div className='flex items-center'>
+                    <button className=" join-item bg-info-content  text-dark hover:bg-info-content rounded-tl-[25px] rounded-bl-[25px] text-sm font-normal !border-0 flex-1 py-2.5 px-[14px] text-left" 
+                    onClick={()=>sendSelectedValue("Min")}>Min</button><input className='border w-full focus:rounded-none h-full px-2' type="text" />
+                    </div>
+                    <span className=" join-item bg-info-content  text-dark hover:bg-info-content font-medium !border-0 shrink-0 px-1 pt-2">to</span>
+                    <div className='flex items-center'>
+                    <input className='border h-full w-full focus:rounded-none px-2' type="text" />
+                    <button className=" join-item bg-info-content h-full text-dark hover:bg-info-content rounded-tr-[25px] rounded-br-[25px] text-sm font-normal flex-1 !border-0 px-[14px] text-right" 
                     onClick={()=>sendSelectedValue("Max")}>Max</button>
+                    </div>
                 </div>
                 <div className="text-right">
                     <Button children={'Apply'} 
