05ba58bbed42a46f400864844fca4b13ec56b89a
venkatalakshmi <venkatalakshmi@yellowblock.net>
2024-06-24 14:43:50 +0530

    Search Movie

--- src/components/marketplace.component/hotcollections.component/SearchComponent.tsx
+++ src/components/marketplace.component/hotcollections.component/SearchComponent.tsx
@@ -3,7 +3,7 @@ import React  from 'react';
 interface SearchInputComponentProps {
   text: string,
   style: any,
-  children?: React.ReactNode 
+  children?: React.ReactNode
   numberOfLines?:number,
   fontFamily?:string,
   handleSearch: (event: React.KeyboardEvent<HTMLInputElement>) => void;
@@ -16,9 +16,9 @@ const SearchInputComponent = ({ text, style, children,numberOfLines,fontFamily,h
 
   return (<>
      <div className='relative xl:w-[42rem] md:w-96 h-[42px]'>
-      <input type="text" placeholder="Search Movie, NFT Name,  Category...... " className="rounded-[30px] focus:outline-none input input-bordered w-full xl:w-[42rem] md:w-96 h-[42px] pr-12" 
+      <input type="text" placeholder="Search NFT Name,  Category...... " className="rounded-[30px] focus:outline-none input input-bordered w-full xl:w-[42rem] md:w-96 h-[42px] pr-12"
       onKeyDown={handleSearch}
-      onChange={handleChange}      
+      onChange={handleChange}
       value={search}/>
        <span className='icon search absolute top-[10px] right-[18px] cursor-pointer' onClick={handleSearchIcon}/>
     </div>
--- src/components/nfts.component/index.tsx
+++ src/components/nfts.component/index.tsx
@@ -240,7 +240,7 @@ const Nfts = forwardRef((props: any, ref) => {
             <div className="container mx-auto pt-5 px-3 lg:px-0">
                 <div className="mt-7 mb-[42px]">
                     <div className="md:flex justify-between gap-4">
-                        <SearchBar searchBarClass='xl:w-[42rem] md:w-96 relative' onSearch={handleSearch} inputRef={searchInputRef} placeholder="Search Movie, NFT Name,  Category...... " />
+                        <SearchBar searchBarClass='xl:w-[42rem] md:w-96 relative' onSearch={handleSearch} inputRef={searchInputRef} placeholder="Search NFT Name,  Category...... " />
                         <div className="flex items-center max-sm:mt-2">
                             <div className="dropdown mr-2.5">
                                 <div tabIndex={0} role="button" className=" m-1 bg-accent px-4 py-2.5 rounded-[28px] text-sm font-medium border-0 hover:bg-accent">Price: {localState.values?.price} <span className="icon drop-arrow"></span></div>
