c5febaadc7c92f9fff9b8a900f3402e124cedb36
Mouneeswar <minnapadv2@gmail.com>
2024-07-01 09:08:01 +0530

    changes in protected route

--- src/layout/protectedRoute.tsx
+++ src/layout/protectedRoute.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import ConnectToWallet from '../components/ConnectToWallet';
+// import ConnectToWallet from '../components/ConnectToWallet';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
     const user=useSelector((store:any)=>store.auth.user)
     if (!user || !user?.id) {
-        return <ConnectToWallet/>;
+        return <Navigate to={'/dashboard'}/>;
     }
     return children;
 };
