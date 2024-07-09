322452e51f9fdebdd3b5d9668243cbf6386f92c1
haridatha <haridatta@yellowblock.net>
2024-07-09 15:45:01 +0530

    about-us

--- src/components/strapi/aboutus/index.tsx
+++ src/components/strapi/aboutus/index.tsx
@@ -7,7 +7,6 @@ import blueellipse from '../../../assets/images/blue-ellipse.svg'
 import VisitUs from '../../visitus';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { setError } from '../../../reducers/layoutReducer';
-import Spinner from '../../loaders/spinner';
 import { clearAboutPageData, getAboutPageDeatils } from '../docs/strapiReducer';
 import AboutUsShimmer from '../../loaders/aboutusshimmer';
 
@@ -25,32 +24,34 @@ function AboutUs(props:any) {
   }, []);
 
   if (aboutpage?.error) rootDispatch(setError(aboutpage?.error));
+
+  
   return (
     <div>
       {aboutpage.loading && <div className='flex justify-center items-center mb-4'>
         <AboutUsShimmer/>
         </div>}
-      {!aboutpage.loading && aboutpage?.data?.map((item) => (<>
+      {!aboutpage.loading && aboutpage?.data &&  aboutpage?.data?.map((item:any) => (<>
         <div className="bg-[#E7F4FA] bg-about relative">
           <div className='container mx-auto grid md:grid-cols-2 items-center px-3 lg:px-0'>
             <div>
-              <h1 className='text-[40px] font-semibold text-dark'>{item.attributes?.abouttitle} <span className='text-primary'>{item.attributes.primarytitle}</span> {item.attributes.suffixtitle} </h1>
-              <p className='text-base  font-normal mt-4 text-dark'>{item.attributes?.ybDescription}</p>
+              <h1 className='text-[40px] font-semibold text-dark'>{item?.attributes?.abouttitle} <span className='text-primary'>{item?.attributes?.primarytitle}</span> {item?.attributes?.suffixtitle} </h1>
+              <p className='text-base  font-normal mt-4 text-dark'>{item?.attributes?.ybDescription}</p>
               <div className="flex gap-16 my-6">
                 <div>
-                  <h1 className='text-[48px] font-semibold text-dark'>{item.attributes?.expvalue}</h1>
-                  <p className='text-base font-medium text-primary'>{item.attributes?.explabel}</p>
+                  <h1 className='text-[48px] font-semibold text-dark'>{item?.attributes?.expvalue}</h1>
+                  <p className='text-base font-medium text-primary'>{item?.attributes?.explabel}</p>
                 </div>
                 <div>
-                  <h1 className='text-[48px] font-semibold text-dark'>{item.attributes?.devvalue}</h1>
-                  <p className='text-base font-medium text-primary'>{item.attributes.devlabel}</p>
+                  <h1 className='text-[48px] font-semibold text-dark'>{item?.attributes?.devvalue}</h1>
+                  <p className='text-base font-medium text-primary'>{item?.attributes?.devlabel}</p>
                 </div>
               </div>
-              <Button type='primary' children={item.attributes?.contact?.Title} btnClassName='md:w-[160px]' />
-              <Button type='cancel' children={item.attributes?.knowmore?.Title} btnClassName='ml-4 md:w-[160px] whitespace-nowrap' />
+              <Button type='primary' children={item?.attributes?.contact?.Title} btnClassName='md:w-[160px]' />
+              <Button type='cancel' children={item?.attributes?.knowmore?.Title} btnClassName='ml-4 !text-black border-[#000] md:w-[160px] whitespace-nowrap' />
             </div>
             <div>
-              <img src={item.attributes.bannerimg.data?.attributes?.url} alt="" width='' className='md:w-[564px] ml-auto' />
+              <img src={item?.attributes?.bannerimg?.data?.attributes?.url} alt="" width='' className='md:w-[564px] ml-auto' />
             </div>
           </div>
           <img src={abstract1} alt="" className='absolute bottom-[-180px]' />
@@ -60,29 +61,29 @@ function AboutUs(props:any) {
           <div className='grid lg:grid-cols-2 items-center container mx-auto'>
             <div>
               <span className='inline-block bg-primary h-1 w-[54px] mb-7'></span>
-              <h1 className='text-[40px] font-semibold text-secondary'>{item.attributes?.ybs} <span className='text-primary'>{item.attributes.custom}</span> {item.attributes.sstitle} </h1>
-              <p className='text-base text-base-200 font-normal mt-4'>{item.attributes?.Description}</p>
+              <h1 className='text-[40px] font-semibold text-secondary'>{item?.attributes?.ybs} <span className='text-primary'>{item?.attributes?.custom}</span> {item?.attributes?.sstitle} </h1>
+              <p className='text-base text-base-200 font-normal mt-4'>{item?.attributes?.Description}</p>
             </div>
             <div className='lg:absolute mt-4 lg:mt-0 right-0 top-16 lg:w-[600px] bg-primary-content blockchain-shadow px-14 py-7'>
               <div className="md:flex items-center gap-10">
-                <img src={item.attributes.bcimg.data?.attributes?.url} alt="" className='w-[91px] mb-2 md:mb-0' />
+                <img src={item?.attributes?.bcimg?.data?.attributes?.url} alt="" className='w-[91px] mb-2 md:mb-0' />
                 <div>
-                  <h1 className='text-secondary text-xl font-medium'>{item.attributes?.bctitle}</h1>
-                  <p className='text-base text-base-200 font-normal'>{item.attributes?.bcdesc}</p>
+                  <h1 className='text-secondary text-xl font-medium'>{item?.attributes?.bctitle}</h1>
+                  <p className='text-base text-base-200 font-normal'>{item?.attributes?.bcdesc}</p>
                 </div>
               </div>
               <div className="md:flex items-center gap-10 my-10">
-                <img src={item.attributes.fsimg.data?.attributes?.url} alt="" className='mb-2 md:mb-0' />
+                <img src={item?.attributes?.fsimg?.data?.attributes?.url} alt="" className='mb-2 md:mb-0' />
                 <div>
-                  <h1 className='text-secondary text-xl font-medium'>{item.attributes?.fstitle}</h1>
-                  <p className='text-base text-base-200 font-normal'>{item.attributes?.fsdesc}</p>
+                  <h1 className='text-secondary text-xl font-medium'>{item?.attributes?.fstitle}</h1>
+                  <p className='text-base text-base-200 font-normal'>{item?.attributes?.fsdesc}</p>
                 </div>
               </div>
               <div className="md:flex items-center gap-10">
-                <img src={item.attributes.ssimg.data?.attributes?.url} alt="" className='mb-2 md:mb-0' />
+                <img src={item?.attributes?.ssimg?.data?.attributes?.url} alt="" className='mb-2 md:mb-0' />
                 <div>
-                  <h1 className='text-secondary text-xl font-medium'>{item.attributes?.sstitle}</h1>
-                  <p className='text-base text-base-200 font-normal'>{item.attributes?.ssdesc}</p>
+                  <h1 className='text-secondary text-xl font-medium'>{item?.attributes?.sstitle}</h1>
+                  <p className='text-base text-base-200 font-normal'>{item?.attributes?.ssdesc}</p>
                 </div>
               </div>
             </div>
@@ -90,33 +91,33 @@ function AboutUs(props:any) {
         </div>
         <div className="relative">
           <div className='pt-48 container mx-auto'>
-            <h1 className='text-[40px] font-semibold text-secondary text-center mb-8'>{item.attributes?.chooseyb} <span className='text-primary'>YellowBlock</span>? </h1>
+            <h1 className='text-[40px] font-semibold text-secondary text-center mb-8'>{item?.attributes?.chooseyb} <span className='text-primary'>YellowBlock</span>? </h1>
             <div className='grid lg:grid-cols-3 gap-4 px-3 lg:px-0'>
               <div className='border border-[#E8E7E7] rounded-2xl py-6 px-9 bg-primary-content choose-shadow'>
-                <img src={item.attributes.visionimg.data.attributes.url} alt="" />
-                <h1 className='my-3 text-secondary text-xl font-bold'>{item.attributes?.visiontitle}</h1>
-                <p className='text-base text-base-200 font-normal'>{item.attributes?.visiondesc}</p>
+                <img src={item?.attributes?.visionimg?.data?.attributes?.url} alt="" />
+                <h1 className='my-3 text-secondary text-xl font-bold'>{item?.attributes?.visiontitle}</h1>
+                <p className='text-base text-base-200 font-normal'>{item?.attributes?.visiondesc}</p>
               </div>
               <div className='border border-[#E8E7E7] rounded-2xl py-6 px-9 bg-primary-content choose-shadow'>
-                <img src={item.attributes.expertimg.data.attributes.url} alt="" />
-                <h1 className='my-3 text-secondary text-xl font-bold'>{item.attributes?.exptitle}</h1>
-                <p className='text-base text-base-200 font-normal'>{item.attributes?.expdesc}</p>
+                <img src={item?.attributes?.expertimg?.data?.attributes?.url} alt="" />
+                <h1 className='my-3 text-secondary text-xl font-bold'>{item?.attributes?.exptitle}</h1>
+                <p className='text-base text-base-200 font-normal'>{item?.attributes?.expdesc}</p>
               </div>
               <div className='border border-[#E8E7E7] rounded-2xl py-6 px-9 bg-primary-content choose-shadow'>
-                <img src={item.attributes.commitimg.data.attributes.url} alt="" />
-                <h1 className='my-3 text-secondary text-xl font-bold'>{item.attributes?.committitle}</h1>
-                <p className='text-base text-base-200 font-normal'>{item.attributes?.commitdesc}</p>
+                <img src={item?.attributes?.commitimg?.data?.attributes?.url} alt="" />
+                <h1 className='my-3 text-secondary text-xl font-bold'>{item?.attributes?.committitle}</h1>
+                <p className='text-base text-base-200 font-normal'>{item?.attributes?.commitdesc}</p>
               </div>
             </div>
           </div>
-          <img src={yellowellipse} className='absolute top-0 z-[-1] ' alt="" />
-          <img src={blueellipse} className='right-0 absolute bottom-[-200px] z-[-1] ' alt="" />
+          {/* <img src={yellowellipse} className='absolute top-0 z-[-1] ' alt="" />
+          <img src={blueellipse} className='right-0 absolute bottom-[-200px] z-[-1] ' alt="" /> */}
         </div>
-        <div className="grid lg:grid-cols-4 gap-12 lg:gap-4 justify-center lg:justify-between container mx-auto mt-16 px-3 lg:px-0">
-          <img src={item.attributes.fireblock.data.attributes?.url} alt="" />
-          <img src={item.attributes.metamask.data.attributes?.url} alt="" />
-          <img src={item.attributes.web3.data.attributes?.url} alt="" />
-          <img src={item.attributes.blockfill.data.attributes?.url} alt="" />
+        <div className="grid company-logos lg:grid-cols-4 gap-12 lg:gap-4 justify-center lg:justify-between container mx-auto mt-16 px-3 lg:px-0">
+          <img src={item?.attributes?.fireblock?.data?.attributes?.url} alt="" />
+          <img src={item?.attributes?.metamask?.data?.attributes?.url} alt="" />
+          <img src={item?.attributes?.web3?.data?.attributes?.url} alt="" />
+          <img src={item?.attributes?.blockfill?.data?.attributes?.url} alt="" />
 
         </div>
         <div className='pt-16'>
